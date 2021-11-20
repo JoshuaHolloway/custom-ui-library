b76516f934ba9ded0c4a1116e4fde9ff9d904d1c
JoshuaHolloway <jhollow6@asu.edu>
2021-11-20 11:56:17 -0600

    1st page (after zero'th page) animates correctly to the right

--- page-animation/pages/index.js
+++ page-animation/pages/index.js
@@ -1,8 +1,6 @@
 import { useState, useEffect, useRef, useContext } from 'react';
 import { gsap } from 'gsap';
 
-import Page from '../components/page';
-
 // ==============================================
 
 export default function HomePage() {
@@ -23,8 +21,6 @@ export default function HomePage() {
   const [page, setPage] = useState(0);
   const [prev_page, setPrevPage] = useState();
 
-  const setPageRef = (page_num) => (el) => (page_ref.current[page_num] = el);
-
   // --------------------------------------------
 
   useEffect(() => {
@@ -36,13 +32,13 @@ export default function HomePage() {
           gsap
             .timeline()
             .to(page_ref.current[page - 1], {
-              duration: 0.5,
+              duration: 2.5,
               xPercent: 100,
             })
             .to(
               page_ref.current[page],
               {
-                duration: 0.5,
+                duration: 2.5,
                 xPercent: 100,
               },
               '<'
@@ -86,7 +82,7 @@ export default function HomePage() {
           height: '100%',
         }}
       >
-        <h1>Page 1</h1>
+        <h1>Page 0</h1>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           width='16'
@@ -103,25 +99,135 @@ export default function HomePage() {
       </div>
 
       {/* page-2 */}
-      <Page
-        page_num={1}
-        pageChangeHandler={pageChangeHandler}
-        setPageRef={setPageRef}
-      />
+      <div
+        ref={(el) => (page_ref.current[1] = el)}
+        style={{
+          background: 'deepskyblue',
+          position: 'absolute',
+          top: 0,
+          left: '-100%',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <h1>Page 1</h1>
+
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='16'
+          height='16'
+          fill='currentColor'
+          viewBox='0 0 16 16'
+          onClick={pageChangeHandler(0)}
+        >
+          <path
+            fillRule='evenodd'
+            d='M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z'
+          />
+        </svg>
+
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='16'
+          height='16'
+          fill='currentColor'
+          viewBox='0 0 16 16'
+          onClick={() => {
+            tl_ref.current.push(
+              gsap
+                .timeline()
+                .to(page_ref.current[1], {
+                  duration: 2.5,
+                  xPercent: 200,
+                })
+                .to(
+                  page_ref.current[2],
+                  {
+                    duration: 2.5,
+                    xPercent: 100,
+                  },
+                  '<'
+                )
+            );
+          }}
+        >
+          <path
+            fillRule='evenodd'
+            d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z'
+          />
+        </svg>
+      </div>
 
       {/* page-3 */}
-      <Page
-        page_num={2}
-        pageChangeHandler={pageChangeHandler}
-        setPageRef={setPageRef}
-      />
+      <div
+        ref={(el) => (page_ref.current[2] = el)}
+        style={{
+          background: 'darkorchid',
+          position: 'absolute',
+          top: 0,
+          left: '-100%',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <h1>Page 2</h1>
+
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='16'
+          height='16'
+          fill='currentColor'
+          viewBox='0 0 16 16'
+          onClick={pageChangeHandler(1)}
+        >
+          <path
+            fillRule='evenodd'
+            d='M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z'
+          />
+        </svg>
+
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='16'
+          height='16'
+          fill='currentColor'
+          viewBox='0 0 16 16'
+          onClick={pageChangeHandler(3)}
+        >
+          <path
+            fillRule='evenodd'
+            d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z'
+          />
+        </svg>
+      </div>
 
       {/* page-4 */}
-      <Page
-        page_num={3}
-        pageChangeHandler={pageChangeHandler}
-        setPageRef={setPageRef}
-      />
+      <div
+        ref={(el) => (page_ref.current[3] = el)}
+        style={{
+          background: 'darkorange',
+          position: 'absolute',
+          top: 0,
+          left: '-100%',
+          width: '100%',
+        }}
+      >
+        <h1>Page 3</h1>
+
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='16'
+          height='16'
+          fill='currentColor'
+          viewBox='0 0 16 16'
+          onClick={pageChangeHandler(2)}
+        >
+          <path
+            fillRule='evenodd'
+            d='M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z'
+          />
+        </svg>
+      </div>
     </div>
   );
 }
