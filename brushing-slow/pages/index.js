50ed7e5c38263574b920ecfeb07c7932d6071a57
JoshuaHolloway <jhollow6@asu.edu>
2021-11-22 10:02:17 -0600

    brushing (slow)

--- brushing-slow/pages/index.js
+++ brushing-slow/pages/index.js
@@ -32,8 +32,6 @@ export default function Home() {
 
   const filteredData = brushExtent
     ? data.filter((d) => {
-        console.log('filteredData()');
-
         const date = xValue(d);
         return date > brushExtent[0] && date < brushExtent[1];
       })
