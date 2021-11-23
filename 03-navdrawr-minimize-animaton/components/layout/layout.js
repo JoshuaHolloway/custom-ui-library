a4064fed7713d2c41df6ad914bd45d197c4feee6
JoshuaHolloway <jhollow6@asu.edu>
2021-11-23 11:51:51 -0600

    replaced minimize and maximize svg's with left and right chevron arrow svg's

--- 03-navdrawr-minimize-animaton/components/layout/layout.js
+++ 03-navdrawr-minimize-animaton/components/layout/layout.js
@@ -149,12 +149,16 @@ export default function Layout({ children }) {
           <svg
             ref={navdrawer_svg_min_ref}
             xmlns='http://www.w3.org/2000/svg'
-            width='20'
-            height='20'
+            width='16'
+            height='16'
             fill='currentColor'
+            className='bi bi-chevron-left'
             viewBox='0 0 16 16'
           >
-            <path d='M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5zm5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5zM0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zm10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4z' />
+            <path
+              fillRule='evenodd'
+              d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z'
+            />
           </svg>
         </div>
 
@@ -167,13 +171,17 @@ export default function Layout({ children }) {
           <svg
             ref={navdrawer_svg_max_ref}
             xmlns='http://www.w3.org/2000/svg'
-            width='20'
-            height='20'
+            width='16'
+            height='16'
             fill='currentColor'
+            className='bi bi-chevron-right'
             viewBox='0 0 16 16'
             style={{ opacity: 0 }}
           >
-            <path d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z' />
+            <path
+              fillRule='evenodd'
+              d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'
+            />
           </svg>
         </div>
 
